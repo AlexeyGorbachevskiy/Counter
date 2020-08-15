867cb88e913b54d78c540183964155f6dd013475
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2020-08-15 22:04:41 +0300

    Updates

---
+++ precache-manifest.e03cffbca3cee1e6cb8f13f97c26d098.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a6fb482b1972a6f4ce00e8a2c237e84",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "b072456b8407af8c5896",
+    "url": "/Counter/static/css/main.83ed8177.chunk.css"
+  },
+  {
+    "revision": "64e6ab9a794762fc59fe",
+    "url": "/Counter/static/js/2.52479356.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Counter/static/js/2.52479356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b072456b8407af8c5896",
+    "url": "/Counter/static/js/main.ecaa685e.chunk.js"
+  },
+  {
+    "revision": "8290f2e6b6e6218d3651",
+    "url": "/Counter/static/js/runtime-main.c5ebaff2.js"
+  }
+]);
